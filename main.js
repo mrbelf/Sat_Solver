36870346bd6b3ec8001b62f860138f74ea28a78c
Tiago Moraes <tbm2@cin.ufpe.br>
2018-06-10 14:53:18 -0300

    Funcionou a função solve

---
+++ main.js
@@ -0,0 +1,15 @@
+//imports te modules.
+let sat_solver = require('sat_solver');
+let readline = require('readline-sync');
+
+//asks the user what's the file name.
+let fileName = readline.question("What's the file name?");
+
+//starts counting runtime
+console.time('runtime');
+
+let result = sat_solver.solve(fileName + '.cnf');
+
+console.timeEnd('runtime');
+
+console.log(result);
