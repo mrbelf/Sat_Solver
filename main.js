db05d527d2268fea99517fe3e75b1f3470c31324
Tiago Moraes <tbm2@cin.ufpe.br>
2018-06-11 00:28:31 -0300

    Criada a interface do usuário

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
+console.log('(Type "exit" to exit)')
+console.log('\nTrivial SAT Solver\nMade by Tiago Moraes & Matheus Belfort');
+console.log('-------------------------------------------------------');
 
-//imports te modules.
-let sat_solver = require('sat_solver');
-let readline = require('readline-sync');
+var exit = false;
 
-//asks the user what's the file name.
-let fileName = readline.question("What's the file name?");
+while (!exit == true) {
 
-//starts counting runtime
-console.time('runtime');
+    //imports te modules.
+    let sat_solver = require('sat_solver');
+    let readline = require('readline-sync');
+    
+    //asks the user what's the file name.
+    let fileName = readline.question("What's the file name?\n");
+    console.log('\033c');
+    
+    if (fileName != 'exit') {      
+        console.log('-------------------------------------------------------')
 
-let result = sat_solver.solve(fileName + '.cnf');
+        //starts counting runtime
+        console.time('runtime');
 
-console.timeEnd('runtime');
+        let result = sat_solver.solve(fileName);
+        if (result.isSat == true) {
+            console.log('É SAT!');
+            console.log('Uma solução é: [' + result.satisfyingAssignment + ']');
+        } else {
+            console.log('NÃO É SAT :(')
+        }
+        console.timeEnd('runtime');
+        console.log('-------------------------------------------------------\n\n\n\n')
+        console.log('Test again:');
 
-console.log(result);
+    } else {
+        exit = true;
+    }
+}
